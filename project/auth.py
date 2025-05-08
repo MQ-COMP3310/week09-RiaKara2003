00504f6977bd3a94f231e43eb350993e3baeae93
RiaKara <ria.karakasis1@students.mq.edu.au>
2025-05-08 10:52:37 +1000

    d

--- project/auth.py
+++ project/auth.py
@@ -39,11 +39,13 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
-    if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
-        app.logger.debug("User email already exists")
-        return redirect(url_for('auth.signup'))
+    signup_query = text('select * ftom user where email = user_email')
+    user = db.session.execute(signup_query, {'user_email': email}).all()
+    if len(user)>0:
+       flash('Email address already exists')
+       app.logger.debug("User email already exists")
+       return redirect(url_for('auth.signup'))
+ 
 
     # create a new user with the form data. TODO: Hash the password so the plaintext version isn't saved.
     new_user = User(email=email, name=name, password=password)
